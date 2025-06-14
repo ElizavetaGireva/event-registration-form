354c4e75b2bc62e1762f566a8ceeded006b2ed1d
Гирева Елизавета <eagireva@edu.hse.ru>
2025-06-15 00:38:45 +0300

    начало 3 части

---
+++ framework/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p>&copy; Система авторизации by Гирева Елизавета</p>
+        </footer>
+    );
+};
+
+export default Footer;
